5e8d22e68cbe046726b45e27488c2aebdba1bab9
Sarvani Kandukuri <sarvanik60@gmail.com>
2022-01-24 18:37:25 +0530

    First commit

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,15 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDetails(){
+    return(
+        <>
+            <MeetupDetail 
+                image='https://us.123rf.com/450wm/hulv850627/hulv8506271511/hulv850627151101788/50623260-hall-building-in-college.jpg?ver=6' 
+                title="college meetup" 
+                address="Some Address" description="College Meetup"
+            />
+        </>
+    )
+}
+
+export default MeetupDetails;
